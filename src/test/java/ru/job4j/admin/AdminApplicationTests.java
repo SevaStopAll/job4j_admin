9637cac5714615e2ada5f56d9340a6f91f0408ab
SevaStopAll <sevastopallrh@yandex.ru>
2023-05-02 13:28:56 +0400

    init commit

---
+++ src/test/java/ru/job4j/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
